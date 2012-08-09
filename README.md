9d50f24e50743ca28a1bbb42a96eac6e383cf366
Renier Morales <renier@morales-rodriguez.net>
2012-08-09 13:03:33 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,21 @@
+## Description
+
+This module creates a graph of your project(s) dependencies that includes version numbers and edges which will be labed according to the type of dependency. That is, devDependencies will say _dev_, optionalDependencies will say _optional_, and so on, except standard dependency edges which will not be labeled. A package found multiple times in the tree with multiple versions, will appear multiple times in the graph, once for each unique version.
+
+It also creates an html table of the dependencies. More info below.
+## Installation
+
+npm install -g graphdeps
+
+## Usage
+
+Go into the directory that **contains** your npm project(s) and:
+```
+graphdeps
+```
+
+A file called _deps.png_ will be created in the current directory with a graph of the project(s) dependncies. Another file called _deps.html_ will be created with the html table listing the dependencies information.
+
+## License
+
+See LICENSE file.
---
+++ bin/graphdeps.js
@@ -0,0 +1,196 @@
+#!/usr/bin/env node
+/**
+ * Scan for module dirs (those with a package.json file)
+ * Skip symbolic links.
+ *
+ * For each module dir:
+ * Note dependencies, bundleDeps, devDeps, and optionalDeps.
+ * Add to output.
+ * scan for module dirs under node_modules.
+ */
+
+var fs = require('fs'),
+    path = require('path'),
+    g = require('graphviz').digraph('G'),
+    edges = {},
+    //DEPTYPES = ["", "dev"],
+    DEPTYPES = [""],
+    REALDEPTYPES = DEPTYPES.map(function(d) { return !d ? "dependencies" : d+"Dependencies"; }),
+    EXCLUDES = ["grunt-ibm-n3"],
+    //EXCLUDES = [],
+    html = "";
+
+function getPackageJson(modulePath) {
+    try {
+        return JSON.parse(fs.readFileSync(path.join(modulePath, "package.json")));
+    } catch(e) {
+        return null;
+    }
+}
+
+function addHtml(json, isRuntime) {
+    var a, homepage;
+    html += "<tr>";
+    if (json.homepage) {
+        homepage = typeof json.homepage === 'string' ? json.homepage : json.homepage[0];
+        html += "<td><a href=\"" + homepage + "\">" + json.name + "</a></td>";
+    } else {
+        html += "<td>" + json.name + "</td>";
+    }
+    html += "<td>" + json.version + "</td>";
+    if (json.license) {
+        if (typeof json.license === 'string') {
+            html += "<td>" + json.license + "</td>";
+        } else {
+            html += "<td><a href=\"" + json.license.url + "\">" + json.license.type + "</a></td>";
+        }
+    } else if (json.licenses && json.licenses.length > 0) {
+        a = [];
+        html += "<td>";
+        json.licenses.forEach(function(license) {
+            if (typeof license === 'string') {
+                a.push(license);
+            } else {
+                a.push("<a href=\"" + license.url + "\">" + license.type + "</a>");
+            }
+        });
+        html += a.join("|");
+        html += "</td>";
+    } else {
+        html += "<td>&nbsp;</td>";
+    }
+    html += "<td>" + (json.description || "") + "</td>";
+    html += "<td>" + (isRuntime ? "Yes" : "No") + "</td>";
+    html += "<td>&nbsp;</td>";
+    html += "</tr>\n";
+}
+
+function grokDeps(dir, forceDev, depKind) {
+    var packageJson = getPackageJson(dir),
+        depDesc = forceDev ? "dev" : depKind,
+        name = packageJson.name,
+        version = packageJson.version,
+        sourceId = name + " " + version;
+    
+    depKind = depKind ? depKind + "Dependencies" : "dependencies";
+
+    if (!g.getNode(sourceId)) {
+        g.addNode(sourceId);
+        addHtml(packageJson, depDesc ? false : true);
+    }
+
+    // Add dep info to output
+    if (packageJson[depKind]) {
+        Object.keys(packageJson[depKind]).forEach(function(key) {
+            var targetId = key + " ",
+                targetDir = path.join(dir, "node_modules", key),
+                searchDir, targetJson;
+
+            if (!fs.existsSync(targetDir)) {
+                if (depKind !== "dependencies") {
+                    return; // other kinds of deps nested in are not installed.
+                }
+                // If standard dep was not installed where we thought,
+                // it is probably already installed already higher up the chain.
+                // Find it.
+                searchDir = path.dirname(dir);
+                while (!fs.existsSync(path.join(searchDir, key))) {
+                    if (searchDir === path.dirname(searchDir)) {
+                        return; // reached the top. give up.
+                    }
+                    searchDir = path.dirname(searchDir);
+                }
+                targetDir = path.join(searchDir, key);
+            }
+            targetJson = getPackageJson(targetDir);
+            targetId += targetJson.version;
+
+            if (!g.getNode(targetId)) {
+                g.addNode(targetId);
+                addHtml(targetJson, depDesc ? false : true);
+            }
+            if (edges[sourceId + " | " + targetId] === undefined) {
+                g.addEdge(sourceId, targetId, { label: depDesc });
+                edges[sourceId + " | " + targetId] = null;
+            }
+        });
+    }
+}
+
+function start(contextDir, forceDev) {
+    if (!fs.existsSync(contextDir)) { return; }
+
+    // Scan for module dirs (thos with a package.json file)
+    var dirs = fs.readdirSync(contextDir), targets = [],
+        packageJson = getPackageJson(path.dirname(contextDir));
+
+    // Filter out bad dirs
+    dirs.forEach(function(dir) {
+        var found = false, target;
+        if (dir.indexOf('.') === 0) { return; }
+        if (EXCLUDES.indexOf(dir) !== -1) { return; }
+        target = path.join(contextDir, dir);
+        if (fs.lstatSync(target).isSymbolicLink()) { return; }
+        if (!fs.statSync(target).isDirectory()) { return; }
+        if (!fs.existsSync(path.join(target, 'package.json'))) { return; }
+
+        if (packageJson) {
+            REALDEPTYPES.forEach(function(depType) {
+                if (dir in packageJson[depType]) {
+                    found = true;
+                }
+            });
+            if (!found) { return; }   
+        }
+
+        targets.push(target);
+    });
+
+    targets.forEach(function(dir) {
+        // We want to propagate a dev relationship for all dependencies
+        // of a first dev dependency.
+        var force = false;
+        if (!forceDev) {
+            if (packageJson &&
+                (!packageJson.dependencies || !packageJson.dependencies[path.basename(dir)]) &&
+                (packageJson.devDependencies && packageJson.devDependencies[path.basename(dir)])) {
+                // This is a dev dependency
+                force = true;
+            }
+        } else { force = forceDev; }
+        // For each module dir, note dependencies...
+        DEPTYPES.forEach(grokDeps.bind(global, dir, force));
+        start(path.join(dir, "node_modules"), force);
+    });
+}
+
+start(process.argv[2] || process.cwd());
+console.log(g.nodeCount() + " unique dependencies found.");
+g.output("png", "deps.png");
+console.log("Created graph deps.png.");
+
+// Write html
+html = 
+    '    <table border="1" cellpadding="0" cellspacing="0" style="width: 98%; ">\n' +
+    '        <thead>\n' +
+    '            <tr>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">Name</span></th>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">Version</span></th>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">License</span></th>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">Description</span></th>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">Part of runtime?</span></th>\n' +
+    '                <th scope="col" style="background-color: #000080; ">\n' +
+    '                    <span style="color:#e6e6fa;">Confidence level that we need to ship this with the product? (H, M, L)</span></th>\n' +
+    '            </tr>\n' +
+    '        </thead>\n' +
+    '        <tbody>\n' + 
+    html;
+html += "        </tbody>\n";
+html += "    </table>";
+fs.writeFileSync("deps.html", html);
+console.log("Wrote table data to deps.html.");
